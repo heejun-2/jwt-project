de53334514ecfb56d454af0b8559fd56f19802bb
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-12-30 13:09:39 +0900

    JoinService

---
+++ src/main/java/com/example/jwtproject/service/JoinService.java
@@ -0,0 +1,34 @@
+package com.example.jwtproject.service;
+
+import com.example.jwtproject.dto.JoinDTO;
+import com.example.jwtproject.entity.UserEntity;
+import com.example.jwtproject.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO){
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if(isExist) return;
+
+        UserEntity data = new UserEntity();
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+}
